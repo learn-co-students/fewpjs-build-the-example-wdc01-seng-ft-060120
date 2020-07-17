48e190420a1d255d3423393bd3a253650986f571
steve <stevepesce879@gmail.com>
2020-07-17 12:20:26 -0400

    Done.

--- main.js
+++ main.js
@@ -3,9 +3,53 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded",() => 
+{
+  errorHidden();
+  document.addEventListener("click",likePost);
+});
 
-
-
+function likePost(e)
+{  
+  tgt = e.target
+  if (tgt.className === 'like-glyph')
+  {
+    mimicServerCall()
+    .then((msg) => toggleHeart(tgt,msg))
+    .catch((error) => errorHidden(false,error))
+  }  
+}
+function toggleHeart(span,msg)
+{
+  if (msg === "Pretend remote server notified of action!")
+    {
+      if (span.className === "activated-heart")
+      {
+          span.innerText = EMPTY_HEART;
+          span.className = "";
+      }else
+      {
+          span.innerText = FULL_HEART;
+          span.className = 'activated-heart';
+      }
+    }
+    return msg
+}
+function errorHidden(flag=true,error)
+{
+  const errorDiv = document.getElementById('modal');
+  const p = errorDiv.querySelector('p');  
+  if (flag)
+  {
+    errorDiv.className = 'hidden'
+    p.innerText = "";    
+  }else
+  {
+    errorDiv.className = "";
+    p.innerText = error;
+    setTimeout(errorHidden,5000);    
+  }
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
